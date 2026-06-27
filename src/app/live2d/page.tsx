76f866ab86ebaa66d2a8f7f2aa4fe3a158aa548c
ysbfsy <2056477131@qq.com>
2026-06-27 11:44:02 +0800

    Update layout for Live2DPage component

--- src/app/live2d/page.tsx
+++ src/app/live2d/page.tsx
@@ -4,7 +4,7 @@ import Live2DViewer from './live2d-viewer'
 
 export default function Live2DPage() {
 	return (
-		<div className='flex h-full items-center justify-center py-8'>
+		<div className='flex min-h-screen items-center justify-center px-4 py-6 sm:px-6 lg:px-10'>
 			<Live2DViewer />
 		</div>
 	)
